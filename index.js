b7952d764d25c30315431e8e3f1089e41c9755e4
vladyatsuk <yatsuk.vladislav@gmail.com>
2023-03-02 20:20:35 +0200

    Revert "Modified function to show complex roots"
    This reverts commit d8d7c09515127fc70cbd40b830fd0daf78e3c953.

--- index.js
+++ index.js
@@ -14,11 +14,5 @@ const solveQuadEq = (a, b, c) => {
         const x = -b / (2 * a);
         console.log(`x1 = x2 = ${x}`);
     }
-    else {
-        const reX = -b / (2 * a);
-        const absImX = Math.sqrt(-D) / (2 * a);
-        const x1 = `${reX} - ${absImX}i`;
-        const x2 = `${reX} + ${absImX}i`;
-        console.log(`There are no real roots\nComplex roots:\nx1 = ${x1}\nx2 = ${x2}`);
-    }
+    else console.log('There are no real roots');
 }
