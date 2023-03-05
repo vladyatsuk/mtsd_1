071e11351d8715884deaf4ba14d4b73eee0d4f57
vladyatsuk <yatsuk.vladislav@gmail.com>
2023-03-05 02:46:31 +0200

    Add error handling
    isQuadratic function is used to see if 'a' is equal to '0'.
    validateValue function checks if the value is a number.
    validateDataFormat function checks if the format of a file
    is right(3 numbers separated by a single space, LF/CRLF in the end)
    promptValue and readValue are used to read values from stdin.

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
+const isQuadraticEq = (a) => {
+    if (a === '0') {
+        console.error('Error. \'a\' cannot be 0');
+        return false;
+    }
+    return true;
+}
+
 const solveQuadEq = (a, b, c) => {
     const A = (a === 1) ? '' : a;
     const B = (b === 1) ? '' : b;
@@ -20,29 +28,64 @@ const solveQuadEq = (a, b, c) => {
     }
     else console.log('There are no real roots');
 }
-
-if (process.argv.length === 3) {
-    const fileName = process.argv[2];
-    fs.readFile(path.resolve(__dirname, fileName), 'utf-8', (err, data) => {
-        if (err) throw err;
-        const args = data.split(' ');
-        solveQuadEq(+args[0], +args[1], +args[2]);
-    })
-}
-
-if (process.argv.length === 2) {
+const promptValue = async (prompt) => {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-    const question = (prompt) => new Promise((resolve) => {
-        rl.question(prompt, resolve);
+    return new Promise((resolve) => {
+        rl.question(prompt, (value) => {
+            rl.close();
+            resolve(value);
+        });
     });
+};
+
+const validateValue = (value) => {
+    if (!/^\d+$/.test(value)) {
+        console.error('Error. Value must be a number');
+        return false;
+    }
+    return true;
+}
+
+const validateDataFormat = (data) => {
+    const format = /^\d+\s\d+\s\d+\r?\n$/;
+    return format.test(data);
+}
+
+
+const readValue = async (prompt, validate) => {
+    while (true) {
+        const value = await promptValue(prompt);
+        if (validate(value)) return value;
+    }
+}
+
+if (process.argv.length === 3) {
+    const fileName = process.argv[2];
+    const filePath = path.resolve(__dirname, fileName);
+    if (!fs.existsSync(filePath)) {
+        console.error(`Error. File ${filePath} doesn't exist`);
+        process.exit(1);
+    }
+    const data = fs.readFileSync(path.resolve(__dirname, fileName), 'utf-8');
+    if (validateDataFormat(data)) {
+        const [a, b, c] = data.trim().split(' ');
+        if (a === '0') {
+            console.error('Error. \'a\' cannot be 0');
+            process.exit(1);
+        }
+        solveQuadEq(+a, +b, +c);
+    } else console.error('Error. Invalid file format')
+}
+
+if (process.argv.length === 2) {
     (async () => {
-        const a = await question('a = ');
-        const b = await question('b = ');
-        const c = await question('c = ');
-        rl.close();
+        const a = await readValue('a = ', (value) => validateValue(value) && isQuadraticEq(value));
+        const b = await readValue('b = ', validateValue);
+        const c = await readValue('c = ', validateValue);
         solveQuadEq(+a, +b, +c);
     })();
+
 }
